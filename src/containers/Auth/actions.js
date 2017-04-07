0685d647ef3f1e6ea80fb75619e507eb953bdd6b
Kai Hao <kevin830726@gmail.com>
2017-04-07 20:51:31 +0800

    add session into localstorage

--- src/containers/Auth/actions.js
+++ src/containers/Auth/actions.js
@@ -1,19 +1,34 @@
 import firebase from 'firebase';
 import { SET_AUTH_INFO } from './constants';
 
-export const setAuthInfo = auth => (dispatch, getState) => {
+export const setAuthInfo = auth => async (dispatch, getState) => {
   if (!auth || !auth.customToken) {
     return {};
   }
 
-  return dispatch({
-    type: SET_AUTH_INFO,
-    payload: firebase.auth()
-      .signInWithCustomToken(auth.customToken)
-      .then(() => auth)
-      .catch(err => ({
-        error: true,
-        ...err
-      })),
+  dispatch({
+    type: `${SET_AUTH_INFO}_PENDING`,
   });
+
+  try {
+    await firebase.auth().signInWithCustomToken(auth.customToken);
+  
+    Object.entries(auth)
+      .forEach(([key, value]) => {
+        window.localStorage.setItem(key, value);
+      });
+
+    return dispatch({
+      type: `${SET_AUTH_INFO}_FULFILLED`,
+      payload: auth,
+    });
+  } catch (err) {
+    return dispatch({
+      type: `${SET_AUTH_INFO}_REJECT`,
+      payload: {
+        error: true,
+        ...err,
+      },
+    });
+  }
 };
