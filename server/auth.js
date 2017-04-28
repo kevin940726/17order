4d820705b1143992ba4bc57c4da8b7bf9234f80d
Kai Hao <kevin830726@gmail.com>
2017-04-28 22:37:09 +0800

    ✨ Feat: use firebase hosting

--- server/auth.js
+++ server/auth.js
@@ -1,24 +1,41 @@
 const ApiBuilder = require('claudia-api-builder');
 const fetch = require('isomorphic-fetch');
 const admin = require('firebase-admin');
-const credentials = require('./credentials.json');
 
 const api = new ApiBuilder();
-const { clientId, clientSecret } = credentials.slack;
+const {
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_DATABASE_URL,
+  SLACK_CLIENT_ID,
+  SLACK_CLIENT_SECRET,
+} = process.env;
 
 admin.initializeApp({
-  credential: admin.credential.cert(credentials.firebase),
-  databaseURL: 'https://bot-6f5f1.firebaseio.com',
+  credential: admin.credential.cert({
+    private_key: FIREBASE_PRIVATE_KEY,
+    client_email: FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: FIREBASE_DATABASE_URL,
 });
 
-api.get('/auth', (request) => {
-  const code = request.queryString.code;
+const promisifyHandler = handler => (request, response, next) => {
+  handler(request)
+    .then(response.json)
+    .catch(err => {
+      console.error(err);
+      next(err);
+    });
+};
+
+const authHandler = (request) => {
+  const { code } = request.query;
 
   if (!code) {
-    return; // stop poking my server!
+    return Promise.reject('No code specified'); // stop poking my server!
   }
 
-  return fetch(`https://slack.com/api/oauth.access?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`)
+  return fetch(`https://slack.com/api/oauth.access?client_id=${SLACK_CLIENT_ID}&client_secret=${SLACK_CLIENT_SECRET}&code=${code}`)
     .then(res => res.json())
     .then(auth => {
       // add to slack
@@ -45,6 +62,6 @@ api.get('/auth', (request) => {
           }))
         );
     });
-});
+};
 
-module.exports = api;
+module.exports = promisifyHandler(authHandler);
